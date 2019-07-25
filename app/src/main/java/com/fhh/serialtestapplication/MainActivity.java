8a4b0e86553303fdd399d48bc2b039db72b3ba47
fhh200000 <1041154549@qq.com>
2019-07-25 14:12:16 +0800

    Update:Created connection control.

--- app/src/main/java/com/fhh/serialtestapplication/MainActivity.java
+++ app/src/main/java/com/fhh/serialtestapplication/MainActivity.java
@@ -2,13 +2,147 @@ package com.fhh.serialtestapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import cn.wch.ch34xuartdriver.CH34xUARTDriver;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button abutton;
+    boolean isOpen;
+    int retval,totalrecv;
+    public static final String TAG = "com.fhh.usbdriver";
+    private static final String ACTION_USB_PERMISSION = "com.fhh.usbdriver.USB_PERMISSION";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DriverHolder.driver = new CH34xUARTDriver(
+                (UsbManager) getSystemService(Context.USB_SERVICE), this,
+                ACTION_USB_PERMISSION);
+        if (!DriverHolder.driver.UsbFeatureSupported())// 判断系统是否支持USB HOST
+        {
+            Dialog dialog = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("提示")
+                    .setMessage("您的手机不支持USB HOST，请更换其他手机再试！")
+                    .setPositiveButton("确认",
+                            new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface arg0,
+                                                    int arg1) {
+                                    System.exit(0);
+                                }
+                            }).create();
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+        abutton = findViewById(R.id.button);
+        abutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isOpen) {
+                    retval = DriverHolder.driver.ResumeUsbList();
+                    if (retval == -1)// ResumeUsbList方法用于枚举CH34X设备以及打开相关设备
+                    {
+                        Toast.makeText(MainActivity.this, "打开设备失败!",
+                                Toast.LENGTH_SHORT).show();
+                        DriverHolder.driver.CloseDevice();
+                    } else if (retval == 0){
+                        if (!DriverHolder.driver.UartInit()) {//对串口设备进行初始化操作
+                            Toast.makeText(MainActivity.this, "设备初始化失败!",
+                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "打开" +
+                                            "设备失败!",
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        Toast.makeText(MainActivity.this, "打开设备成功!",
+                                Toast.LENGTH_SHORT).show();
+                        isOpen = true;
+                        //openButton.setText("Close");
+                       // configButton.setEnabled(true);
+                        //writeButton.setEnabled(true);
+                        new readThread().start();//开启读线程读取串口接收的数据
+                    } else {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setIcon(R.mipmap.ic_launcher);
+                        builder.setTitle("未授权限");
+                        builder.setMessage("确认退出吗？");
+                        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO Auto-generated method stub
+//								MainFragmentActivity.this.finish();
+                                System.exit(0);
+                            }
+                        });
+                        builder.setNegativeButton("返回", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO Auto-generated method stub
+
+                            }
+                        });
+                        builder.show();
+
+                    }
+                } else {
+                    //openButton.setText("Open");
+                    //configButton.setEnabled(false);
+                   // writeButton.setEnabled(false);
+                    isOpen = false;
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    DriverHolder.driver.CloseDevice();
+                    totalrecv = 0;
+                }
+            }
+        });
+    }
+    private class readThread extends Thread {
+
+        public void run() {
+
+            byte[] buffer = new byte[4096];
+
+            while (true) {
+
+                Message msg = Message.obtain();
+                if (!isOpen) {
+                    break;
+                }
+
+                int length = DriverHolder.driver.ReadData(buffer, buffer.length);
+                if (length > 0) {
+//					String recv = toHexString(buffer, length);
+//					String recv = new String(buffer, 0, length);
+                    //totalrecv += length;
+//                    String content = String.valueOf(totalrecv);
+//                    String content = new String(buffer);
+//                    String content = hexStringToString(toHexString(buffer,length));
+                    String content = new String(buffer);
+                    msg.obj = content+"";
+                    //handler.sendMessage(msg);
+                }
+            }
+        }
     }
 }
