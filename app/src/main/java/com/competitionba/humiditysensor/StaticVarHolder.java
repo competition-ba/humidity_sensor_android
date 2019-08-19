a50fb3cf04d3e1d2488eaa27e6ce381402cd65ab
fhh200000 <1041154549@qq.com>
2019-08-19 22:47:19 +0800

    Update:代码合并#2(Java & 驱动).

---
+++ app/src/main/java/com/competitionba/humiditysensor/StaticVarHolder.java
@@ -0,0 +1,14 @@
+package com.competitionba.humiditysensor;
+
+public class StaticVarHolder {
+    public static byte[] msg;
+    public static int msglen;
+    public static boolean isNFCActivityOpened;
+    public static CH34xUARTDriver driver;//需要将CH34x的驱动类写在APP类下面，使得帮助类的生命周期与整个应用程序的生命周期是相同的
+    public static byte[] getAvailMsg(){
+        byte[] ret = new byte[msglen];
+        for(int i=0;i<msglen;i++)
+            ret[i] = msg[i];
+        return ret;
+    }
+}
